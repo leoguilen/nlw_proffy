c4d62ebd58bb346f99d742c9061ebbcc85b8dbbc
Leonardo Guilen <leonardoguilen1@gmail.com>
2020-08-04 22:34:28 -0300

    Criando classe App para organizar inicialização do servidor

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,8 @@
-import express from 'express';
-import routes from './routes';
-import cors from 'cors';
+import app from './app';
+import chalk from 'chalk';
 
-const app = express();
+const _PORT = 3333;
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333);
+app.listen(_PORT, () => 
+    console.log(chalk.green(
+        'Server is running...')));
